c427ce6c48b244de7d9c4e87b4c3e29df7cbaf07
laddha-adi <f2016038@pilani.bits-pilani.ac.in>
2018-07-22 02:06:44 +0530

    Login Page

---
+++ src/main/java/Controller/LoginPageController.java
@@ -0,0 +1,130 @@
+package Controller;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import javafx.concurrent.Task;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.apache.wink.json4j.JSONException;
+import org.opendatakit.sync.client.SyncClient;
+
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class LoginPageController implements Initializable {
+    public TextField usernameField;
+    public TextField passwordField;
+    public Button loginButton;
+    public ProgressIndicator progressIndicator;
+    public Label statusLabel;
+    public ImageView configureIcon;
+    public Label configureText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        stopProgress();
+        setStatusText(null);
+
+    }
+
+    private void setStatusText(String status){
+        statusLabel.setText(status);
+    }
+
+    public void loginButtonClicked(){
+        loginButton.setDisable(true);
+        setStatusText(null);
+        startProgress();
+        usernameField.setDisable(true);
+        passwordField.setDisable(true);
+
+        if(usernameField.getText().trim().isEmpty()){
+            setStatusText("Username field can't be empty");
+        }
+        else if (passwordField.getText().trim().isEmpty()){
+            setStatusText("Password field can't be empty");
+        }
+        else{
+            attemptLogin(usernameField.getText().trim(), passwordField.getText().trim());
+        }
+    }
+
+    private void attemptLogin(String username, String password) {
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+
+                updateMessage("Please Wait...");
+                updateProgress(-1, 100);
+
+                try {
+                    //TODO: write code to verify user credentials from server.
+                    if(username.equals("admin")&& password.equals("password")){
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    updateProgress(0, 100);
+                }
+                return null;
+            }
+        };
+        task.setOnSucceeded(taskFinishEvent -> {
+            //TODO: complete onSucceeded method.
+            moveToDashboard();
+        });
+
+        progressIndicator.progressProperty().bind(task.progressProperty());
+        statusLabel.textProperty().bind(task.messageProperty());
+        new Thread(task).start();
+    }
+
+    private void moveToDashboard() {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MainUI.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("My New Stage Title");
+        try {
+            stage.setScene(new Scene(fxmlLoader.load(),    1024, 600));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.show();
+        ((Stage) loginButton.getScene().getWindow()).close();
+    }
+
+    private void startProgress(){
+        progressIndicator.setVisible(true);
+        progressIndicator.setProgress(-1.0);
+    }
+
+    private void stopProgress(){
+        progressIndicator.setVisible(false);
+        progressIndicator.setProgress(0.0);
+    }
+
+    public void configureButtonClicked() {
+       Stage primaryStage = ((Stage) configureText.getScene().getWindow());
+       FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Settings.fxml"));
+        try {
+            primaryStage.setScene(new Scene(fxmlLoader.load(),    740, 420 ));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
