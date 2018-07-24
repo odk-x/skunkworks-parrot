85b3de382ae8cbba9f26e1ba8017eec7badb8f8a
laddha-adi <f2016038@pilani.bits-pilani.ac.in>
2018-07-25 01:20:58 +0530

    Configure Page

--- src/main/java/Controller/SettingsController.java
+++ src/main/java/Controller/SettingsController.java
@@ -1,18 +1,14 @@
 package Controller;
 
+import Data.LoginData;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
-import org.json.JSONObject;
-
-import java.awt.event.MouseEvent;
 import java.io.*;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class SettingsController implements Initializable {
@@ -21,14 +17,26 @@ public class SettingsController implements Initializable {
     public TextField dynamicLinkDomainField;
     public TextField packageNameField;
     public ImageView folderIcon;
-   @FXML public Button saveButton;
+    public TextField syncClientURLField;
+    private HashMap<String, String> configure;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        saveButton.setOnMouseClicked(e->saveButtonClicked());
-        folderIcon.setOnMouseClicked(e->openFileChooser());
+        readKeyData();
     }
 
+    private void readKeyData() {
+       if(LoginData.loginData==null) LoginData.loginData = new LoginData();
+
+       serviceAccountKeyPathField.setText(LoginData.loginData.getSERVICE_ACCOUNT_KEY_PATH());
+       webAPIKeyField.setText(LoginData.loginData.getWEB_API_KEY());
+       dynamicLinkDomainField.setText(LoginData.loginData.getDYNAMIC_LINK_DOMAIN());
+       packageNameField.setText(LoginData.loginData.getANDROID_APP_PACKAGE_NAME());
+       syncClientURLField.setText(LoginData.loginData.getSYNC_CLIENT_URL());
+       System.out.print(LoginData.loginData);
+    }
+
+    @FXML
     private void openFileChooser() {
         FileChooser fileChooser = new FileChooser();
         FileChooser.ExtensionFilter extentionFilter = new FileChooser.ExtensionFilter("JSON files (*.json)", "*.json");
@@ -50,52 +58,11 @@ public class SettingsController implements Initializable {
     }
 
     public void saveButtonClicked()  {
-        String serviceAccountKeyPath = serviceAccountKeyPathField.getText();
-        String webAPIKey = webAPIKeyField.getText();
-        String dynamicLinkDomain = dynamicLinkDomainField.getText();
-        String packageName = packageNameField.getText();
-
-        JSONObject obj = new JSONObject();
-        obj.put("serviceAccountKeyPath", serviceAccountKeyPath);
-        obj.put("webAPIKey", webAPIKey);
-        obj.put("dynamicLinkDomain", dynamicLinkDomain);
-        obj.put("packageName", packageName);
-
-        try{
-            FileWriter file = new FileWriter("keys.json");
-            file.write(obj.toString());
-            System.out.println("Successfully Copied JSON Object to File...\n" + obj.toString());
-            file.close();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-
-        InputStream is = null;
-        OutputStream os = null;
-        try {
-            is = new FileInputStream(serviceAccountKeyPath);
-            os = new FileOutputStream("serviceAccountKey.json");
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = is.read(buffer)) > 0) {
-                os.write(buffer, 0, length);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-                os.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
+        LoginData.loginData.setANDROID_APP_PACKAGE_NAME(packageNameField.getText());
+        LoginData.loginData.setDYNAMIC_LINK_DOMAIN(dynamicLinkDomainField.getText());
+        LoginData.loginData.setSERVICE_ACCOUNT_KEY_PATH(serviceAccountKeyPathField.getText());
+        LoginData.loginData.setWEB_API_KEY(webAPIKeyField.getText());
+        LoginData.loginData.setSYNC_CLIENT_URL(syncClientURLField.getText());
+        LoginData.loginData.saveKeys();
     }
-
-
-
-
 }
